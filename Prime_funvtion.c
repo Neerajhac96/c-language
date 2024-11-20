e42908bbfdfd8c8e079645f5b8b64713ed19570a
NIRAJ KUMAR <arsnd95@gmail.com>
2024-11-20 10:52:19 +0530

    add prime_function

--- Prime_funvtion.c
+++ Prime_funvtion.c
@@ -1,11 +1,21 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+void prime(int num)
+{
+    int i,count=0;
+    for(i=1;i<=num;i++){
+        if(num%i==0)
+        count++;
+    }
+    if(count==2)
+    printf("prime");
+    else
+    printf("%d is not prime" ,num);
+}
+int main()
+{
     int num;
     printf("Enter a number: ");
-    scanf("%d",&num);
-    if(num%2==0)
-        printf("%d is an even number",num);
-    else
-        printf("%d is an odd number",num);
+    scanf("%d", &num);
+    prime(num);
     return 0;
 }
