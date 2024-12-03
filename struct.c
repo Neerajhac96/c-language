1e2d90faa7c730d326d3bf8b879a34e2eca8c8a9
NIRAJ KUMAR <arsnd95@gmail.com>
2024-12-03 22:25:17 +0530

    ADDstruxt prigram

--- struct.c
+++ struct.c
@@ -10,10 +10,23 @@ struct student
 };
 int main(){
     struct student st1;
-    scanf("%s%d%f%c%d",st1.name,st1.ID,st1.cse_marks,st1.gender,st1.phone_no);
-    struct student st2={"dheeraj",543,13.5,'M',1939494};
-    
-    printf("name: %s ID: %d CSE marks: %.1f GEnder: %c Phone: %d\n",st1.name,st1.ID,st1.cse_marks,st1.gender,st1.phone_no);
-    printf("name: %s ID: %d CSE marks: %.1f GEnder: %c Phone: %d\n",st2.name,st2.ID,st2.cse_marks,st2.gender,st2.phone_no);
+    printf("Enter Employee Name: ");
+    fgets(st1.name, sizeof(st1.name), stdin);
+    printf("Enter ID Number");
+    scanf("%d",&st1.ID);
+    printf("Enter CSE Marks: ");
+    scanf("%f",&st1.cse_marks);
+    printf("Enter Gender (M/F): ");
+    scanf(" %c",&st1.gender);
+    printf("ENter phone number: ");
+    scanf("%d",&st1.phone_no);
+    printf("\nEmployee Details:\n");
+    printf("Name:         %s",st1.name);
+    printf("ID:           %d\n",st1.ID);
+    printf("CSE Marks:    %.2f\n",st1.cse_marks);
+    printf("Gender:       %c\n",st1.gender);
+    printf("Phone Number: %ld\n",st1.phone_no);
+
+
     return 0;
 }
