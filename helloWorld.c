6b8d14f55ee118e91cecefa78b11b6283f0f6cc4
NIRAJ KUMAR <arsnd95@gmail.com>
2024-12-05 16:15:47 +0530

    reverse array

--- helloWorld.c
+++ helloWorld.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
-#include<string.h>
-struct employee
-{
-    int code;
-    char name[50];
-    float salary;
-};
+int main(){
+    int arr[10],i;
+    printf("Enter element in an array");
+    for(i=0;i<10;i++){
+        scanf("%d",&arr[i]);
+    }
+    printf("Elements in the array in reverse order are:");
+    for(i=0;i<10;i++)
+    printf("%d ",arr[9-i]);
+}
