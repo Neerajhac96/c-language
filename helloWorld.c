cd06f0639a4fd2de5f1987e5c8e40a0313e67bcf
NIRAJ KUMAR <arsnd95@gmail.com>
2024-12-05 14:07:13 +0530

    structure employee search

--- helloWorld.c
+++ helloWorld.c
@@ -6,27 +6,3 @@ struct employee
     char name[50];
     float salary;
 };
-int main(){
-    struct employee emp[3];
-    int i;
-    printf("Enter employee details:\n");
-    for(i=0; i<3; i++)
-    {
-        printf("Employee %d:\n",i+1);
-        printf("Enter employee code: ");
-        scanf("%d",&emp[i].code);
-        printf("Enter employee name: ");
-        scanf("%s",emp[i].name);
-        printf("Enter employee salary: ");
-        scanf("%f",&emp[i].salary);
-    }
-    printf("\nEmployee details:\n");
-    for(i=0; i<3; i++)
-    {
-        printf("Detail of Employee %d:\n",i+1);
-        printf("Employee code:     %d\n",emp[i].code);
-        printf("Employee name:     %s\n",emp[i].name);
-        printf("Employee salary:   %.2f\n",emp[i].salary);
-    }
-    return 0;
-}
---
+++ search-employee-structure.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<string.h>
+struct employee
+{
+    int code;
+    char name[50];
+    float salary;
+};
+int main(){
+    struct employee emp[3];
+    int i;
+    printf("Enter employee details:\n");
+    for(i=0; i<3; i++)
+    {
+        printf("Employee: %d\n",i+1);
+        printf("Enter employee code: ");
+        scanf("%d",&emp[i].code);
+        printf("Enter employee name: ");
+        scanf("%s",emp[i].name);
+        printf("Enter employee salary: ");
+        scanf("%f",&emp[i].salary);
+    }
+    printf("\nEmployee details:\n");
+    for(i=0; i<3; i++)
+    {
+        printf("Detail of Employee: %d\n",i+1);
+        printf("Employee code:     %d\n",emp[i].code);
+        printf("Employee name:     %s\n",emp[i].name);
+        printf("Employee salary:   %.2f\n",emp[i].salary);
+    }
+    int key,pos=0,flag=1;
+    printf("Enter Employee code to br search: ");
+    scanf("%d",&key);
+    for(i=0;i<3;i++){
+        if(emp[i].code==key){
+            flag=1;
+            pos=i;
+        }
+    }
+    if(flag==1){
+        printf("Employee code: %d\n",emp[pos].code);
+        printf("Employee name: %s\n",emp[pos].name);
+        printf("Employee email: %.2f\n",emp[pos].salary);
+    }
+    return 0;
+}
